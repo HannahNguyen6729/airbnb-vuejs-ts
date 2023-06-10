430f692508eedb0026f5511761f51b5daf79dfac
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-06-10 15:57:43 +0300

    add TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <nav>
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
-  </nav>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+
+  <!-- Header Container
+================================================== -->
+  <the-header></the-header>
+  <!-- Header Container / End -->
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheHeader from "./components/TheHeader.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { TheHeader },
+});
+</script>
 
 <style lang="scss"></style>
