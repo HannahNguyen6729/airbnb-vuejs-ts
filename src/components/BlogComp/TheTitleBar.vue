220c29d8e75784987490371274407a4a2639618e
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-06-10 17:14:05 +0300

    add TheContent, TheTitleBar components to Blog page

---
+++ src/components/BlogComp/TheTitleBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="titlebar" class="gradient">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2>Blog</h2>
+          <span>Latest News</span>
+
+          <!-- Breadcrumbs -->
+          <nav id="breadcrumbs">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li>Blog</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheTitleBar",
+});
+</script>
+
+<style></style>
