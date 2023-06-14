c947d7cd8738acb4638d18dbe3525a6df013f8e5
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-06-14 09:02:24 +0300

    custom layout for main search location component

--- src/components/TheMainSearch.vue
+++ src/components/TheMainSearch.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="main-search-input">
-    <div class="main-search-input-item">
+    <!-- <div class="main-search-input-item">
       <input type="text" placeholder="What are you looking for?" value="" />
-    </div>
+    </div> -->
 
     <div class="main-search-input-item location">
       <div id="autocomplete-container">
         <input id="autocomplete-input" type="text" placeholder="Location" />
       </div>
       <a href="#"><i class="fa fa-map-marker"></i></a>
+      <ul class="list-location">
+        <li>
+          <span class="im im-icon-Location-2"></span>
+          <span class="location-item-title">Nha Trang </span>
+        </li>
+        <li>
+          <span class="im im-icon-Location-2"></span>
+          <span class="location-item-title">Can Tho </span>
+        </li>
+      </ul>
     </div>
-
+    <!-- 
     <div class="main-search-input-item">
       <select data-placeholder="All Categories" class="chosen-select">
         <option>All Categories</option>
@@ -20,7 +30,7 @@
         <option>Fitness</option>
         <option>Events</option>
       </select>
-    </div>
+    </div> -->
 
     <button
       class="button"
@@ -37,4 +47,31 @@ import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.main-search-input-item.location {
+  position: relative;
+  .list-location {
+    position: absolute;
+    top: 150%;
+    list-style: none;
+    background: #fff;
+    color: #5e5c5c;
+    width: 100%;
+    padding: 7px;
+    border-radius: 20px;
+    z-index: 1;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    li {
+      padding: 10px;
+      font-size: 20px;
+      &:hover {
+        cursor: pointer;
+        background: #f7f7f7;
+      }
+      .location-item-title {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
